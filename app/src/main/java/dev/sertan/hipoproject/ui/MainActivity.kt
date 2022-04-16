d911a6eaef193097e749db59902af4c53eadb502
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2022-04-16 19:09:58 +0300

    First commit

---
+++ app/src/main/java/dev/sertan/hipoproject/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.sertan.hipoproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.sertan.hipoproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
